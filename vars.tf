09538ece078af88c6ca6519a4f067008bfd69a3c
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-27 16:50:25 -0400

    Terraform Script

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "tags" {}
+variable "env" {}
+variable "component" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
+variable "zone_id" {}
