2876623467a28a3082de107230760c618022013a
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-28 16:15:36 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_lb_target_group" "public" {
 
 resource "aws_lb_target_group_attachment" "public" {
   count = length(tolist(data.dns_a_record_set.private_alb.addrs))
-  target_group_arn = aws_lb_target_group.public[0].arn
+  target_group_arn = aws_lb_target_group.public.arn
   target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index)
   port = 80
 }
