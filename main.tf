2666761c9316f5b74e267a9ffcbd87cfd569c491
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-28 16:17:20 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -102,8 +102,8 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count = length(tolist(data.dns_a_record_set.private_alb.addrs))
-  target_group_arn = aws_lb_target_group.public.arn
+  count = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  target_group_arn = aws_lb_target_group.public[0].arn
   target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index)
   port = 80
 }
