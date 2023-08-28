859fe81588fcdeddadd79e4c8d03c5e56e6a0160
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-28 15:42:33 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -88,7 +88,15 @@ resource "aws_lb_listener_rule" "main" {
 
   condition {
     host_header {
-      values = ["${var.component}-${var.env}.naveen3607.online"]
+      values = [var.component == "frontend" ? "${var.env}.naveen3607.online" : "${var.component}-${var.env}.naveen3607.online"]
     }
   }
 }
+
+#resource "aws_lb_target_group" "public" {
+#  count = var.component == "frontend" ? 1 : 0
+#  name     = local.name_prefix
+#  port     = var.port
+#  protocol = "HTTP"
+#  vpc_id   = var.vpc_id
+#}
