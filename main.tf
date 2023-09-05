9f835fd945eaf710060a99d3fe647fdf73797063
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-05 16:11:46 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -36,6 +36,16 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_security_group_rule" "nginx_exporter" {
+  count = var.component == frontend ? 1 : 0
+  from_port         = 9113
+  protocol          = "tcp"
+  security_group_id = aws_security_group.main.id
+  to_port           = 9113
+  type              = "ingress"
+  cidr_blocks = var.monitoring_ingress_cidr
+}
+
 resource "aws_iam_policy" "main" {
   name        = "${local.name_prefix}-policy"
   path        = "/"
