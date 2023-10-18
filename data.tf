b49d1e20c13dd86496af43286c7098f646d34946
naveen3607 <naveenreddykallam3607@gmail.com>
2023-10-18 17:17:44 -0400

    Terraform Script

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
-data "aws_ami" "ami" {
+/*data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
+}*/
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["207153251982"]
 }
 
 data "dns_a_record_set" "private_alb" {
