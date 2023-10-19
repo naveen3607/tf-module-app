cad7c636f505e55ddad605743c0387b2de0b7444
naveen3607 <naveenreddykallam3607@gmail.com>
2023-10-19 17:48:38 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -158,9 +158,9 @@ resource "aws_autoscaling_group" "main" {
 }
 
 resource "aws_autoscaling_policy" "asg-cpu-rule" {
-  autoscaling_group_name = "CPULoadDetect"
-  name                   = aws_autoscaling_group.main.name
-  policy_type            = "TargetTrackingScaling"
+  name                      = "CPULoadDetect"
+  autoscaling_group_name    = aws_autoscaling_group.main.name
+  policy_type               = "TargetTrackingScaling"
   estimated_instance_warmup = 120
   target_tracking_configuration {
     predefined_metric_specification {
